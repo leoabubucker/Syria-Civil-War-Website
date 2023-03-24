a7a4ef6acda1b5092e9f70e7dc8ce5537fcc5ec4
Leo Abubucker <leokumar@outlook.com>
2023-03-23 22:28:51 -0400

    init commit

---
+++ general.js
@@ -0,0 +1,72 @@
+// For use on element that activates menu ONLY
+
+function toggleMenu(){
+menu = document.getElementById('sideMenu');
+if(menu.style.display == 'grid'){
+    menu.style.display = 'none';
+}
+else{
+    menu.style.display = 'grid';
+    document.getElementById("hidden-overlay").style.display = 'flex';
+    
+}
+}
+
+// For use on body tag or other all-encompassing tag. 
+// Not for use on element that activates menu
+function hideMenu(){
+menu = document.getElementById('sideMenu');
+if(menu.style.display == 'grid'){
+    menu.style.display = 'none';
+    document.getElementById("hidden-overlay").style.display = 'none';
+}
+}
+
+
+function infoReveal(idx){
+    cover = document.getElementsByClassName('cover')[idx];
+    content = document.getElementsByClassName('content')[idx];
+    // cover.style.display = 'none'; 
+    cover.style.animation = "fadeOut 1s ease-in forwards"
+    // content.style.opacity = '1';
+    content.style.animation = "textOpacityUp 1s ease-in forwards"
+}
+function infoHide(idx){
+    cover = document.getElementsByClassName('cover')[idx];
+    content = document.getElementsByClassName('content')[idx];
+    cover.style.animation = "fadeIn 1s ease-in forwards"
+    // cover.style.display = 'flex'; 
+    // content.style.opacity = '0.1';
+    content.style.animation = "textOpacityReset 1s ease-in forwards"
+}
+
+
+function togglePopup(popup_type) {
+    document.getElementById(popup_type).classList.toggle("active");
+}
+
+
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+        document.getElementById("nav").style.backgroundColor = "black";
+        document.getElementById("nav").style.transition = "500ms all ease-in-out";
+
+        document.getElementById("cNav").style.opacity = "1";
+        document.getElementById("lNav").dataset.scroll = "true";
+        document.getElementById("cNav").dataset.scroll = "true";
+        document.getElementById("rNav").dataset.scroll = "true";
+        // document.getElementById("navlink4").dataset.scroll = "true";
+        document.getElementById("contactButton").dataset.scroll = "true";
+    }
+    else {
+        document.getElementById("nav").style.backgroundColor = "unset";
+        document.getElementById("cNav").style.opacity = "0";
+        delete document.getElementById("contactButton").dataset.scroll
+        delete document.getElementById("lNav").dataset.scroll;
+        delete document.getElementById("cNav").dataset.scroll
+        delete document.getElementById("rNav").dataset.scroll;
+        // delete document.getElementById("navlink4").dataset.scroll;
+    }
+
+})
